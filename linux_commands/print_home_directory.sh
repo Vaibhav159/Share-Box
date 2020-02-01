0f2caf751e35902d95a97fb9c9625ef604a150fc
vaibhav159 <vlodha98@gmail.com>
2020-02-01 04:58:19 +0000

    ME_QBOX_MODULE_LINUX_COMMANDS

--- linux_commands/print_home_directory.sh
+++ linux_commands/print_home_directory.sh
@@ -5,5 +5,7 @@
     # 1) Add commands to print the home directory for crio-user
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output
+    eval echo "~$USER"
+
 
 
